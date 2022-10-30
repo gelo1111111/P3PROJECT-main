c3e56256944b5015047c621ad02b4632ef9da583
gelo1111111 <116861103+gelo1111111@users.noreply.github.com>
2022-10-30 22:22:49 +0800

    Initial commit

---
+++ UniqueID.java
@@ -0,0 +1,16 @@
+package p3project;
+public class UniqueID 
+{
+   private int uid;
+    
+    UniqueID(int id)
+    {
+        this.uid = id;
+    }
+    
+    public int getuid()
+    {
+        return uid;
+    }
+} 
+
